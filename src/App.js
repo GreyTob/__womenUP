a866007fe705342b73e4307d925e9322013799f4
Sergey <GreyTob@inbox.ru>
2021-07-01 20:32:43 +0500

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useState } from 'react'
+import { Context } from './index'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+// import firebase from 'firebase'
+import 'firebase/firestore'
 
 function App() {
+  const { firestore } = useContext(Context)
+  const [sends, loader] = useCollectionData(
+    firestore.collection('sends').orderBy('data')
+  )
+  const [value, setValue] = useState('')
+  const [name, setName] = useState('')
+
+  /**
+   * функция добавляет объект с именем пользователя, сообщением и датой в firestore коллекцию, после чего отчищает поля ввода. Поле date используется для сортировки коллекции по дате отправления сообщения
+   *@param {string} name имя пользователя
+   *@param {string} value текст сообщения
+   *@param {number} data текст сообщения
+   */
+  const sendMessage = async (name, value, data) => {
+    firestore.collection('sends').add({
+      name,
+      value,
+      data,
+    })
+    setValue('')
+    setName('')
+  }
+
+  if (loader) {
+    return 'загрузка'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <label htmlFor="youName">Ваше имя:</label>
+        <input
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+          type="text"
+          id="youName"
+        />
+      </div>
+      <div>
+        <label htmlFor="youMessage">Ваше сообщение:</label>
+        <br />
+        <textarea
+          id="youMessage"
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+        ></textarea>
+      </div>
+      <button onClick={sendMessage.bind(this, name, value, +new Date())}>
+        Отправить
+      </button>
+
+      <ul className="messanges">
+        {sends.map((send, index) => {
+          return (
+            <li
+              key={index}
+              className="oneMessage"
+            >{`${send.name}: ${send.value}`}</li>
+          )
+        })}
+      </ul>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { createContext } from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDwiiCwRG_bAwlmVoQXYgHBSGDExZ9vIYQ',
+  authDomain: 'test-wup-9d3da.firebaseapp.com',
+  projectId: 'test-wup-9d3da',
+  storageBucket: 'test-wup-9d3da.appspot.com',
+  messagingSenderId: '204649464963',
+  appId: '1:204649464963:web:ba24f0c5902a0aeb3e47b6',
+  measurementId: 'G-D9N6V7F483',
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+// firebase.analytics()
+
+export const Context = createContext(null)
+const firestore = firebase.firestore()
+
+const app = (
+  <Context.Provider value={{ firebase, firestore }}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Context.Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
